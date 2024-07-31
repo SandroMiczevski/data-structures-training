16a2748006968583c2874cb1597556b1d7001fcd
Sandro Miczevski Jr <sandro.mck@outlook.com>
2024-07-30 23:04:23 -0700

    Finished the methods in the Heap folder.

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -28,9 +28,66 @@ public class Heap {
         }
     }
 
+    public Integer remove(){
+        /***
+         * The standard behaviour of this funtion is to remove the root of the heap, or the element on the top of the tree and then rebalance it.
+         * 
+         * This function will remove the root of the tree, replace it with the minimum value and then rebalance it.
+         * 
+         * @params this function does not accept any parameters.
+         * @return returns the value of the element removed from the list.
+         */
 
+        if (heap.size() == 0){ 
+            //If it's empty, return NULL
+            return null;
+        }
+        else if (heap.size() == 1){
+            // If there's only one element, return that element and make the list null
+            Integer tmp = heap.remove(0);
+            heap = null;
+            return tmp;
+        }
+        else{
+            // For a complete heap tree, remove the main node and put the last element in its place. 
+            // After that, rebalance the heap.
+
+            //Start with returning the value of the top element and then remove it from the tree, swapping with the last one;
+            int maxValue = heap.get(0);
+            
+            heap.set(0, heap.remove( heap.size() - 1));
+
+            //After that is done, calls the SinkDown function.
+            sinkDown(0);
+
+            return maxValue;
+        }
+    }
 
     // Helper methods
+
+    private void sinkDown(int index){
+        
+        int maxValue = index;
+        int heapSize = heap.size();
+
+        while (true){
+            int leftChild = leftChildIndex(index);
+            int rightChild = rightChildIndex(index);
+
+            if (leftChild < heapSize && heap.get(leftChild) > heap.get(maxValue)){
+                maxValue = leftChild;
+            }
+            if (rightChild < heapSize && heap.get(rightChild) > heap.get(maxValue)){
+                maxValue = rightChild;
+            }
+            if (heap.get(maxValue) > heap.get(index)){
+                swap(index, maxValue);
+                index = maxValue;
+                
+            } else {return;}
+        }
+    }
 
     public List<Integer> getHeap(){
         /** Returns a copy of the heap to avoid changes in the main structure */
--- src/Heap/Main.java
+++ src/Heap/Main.java
@@ -5,6 +5,20 @@ public class Main {
     public static void main(String[] args) {
         Heap H = new Heap();
 
+        H.insert(50);
+        H.insert(55);
+        H.insert(65);
+        H.insert(60);
+        H.insert(75);
+        H.insert(80);
+
+        System.out.println(H.getHeap());
+
+        H.remove();
+
+        System.out.println(H.getHeap());
+
+/* 
         H.insert(99);
         H.insert(72);
         H.insert(61);
@@ -19,7 +33,16 @@ public class Main {
         H.insert(200);
         H.insert(60);
         H.insert(59);
+        
+        System.out.println(H.getHeap());
+
+        H.remove();
 
         System.out.println(H.getHeap());
+
+        
+        H.remove();
+
+        System.out.println(H.getHeap());*/
     }
 }
